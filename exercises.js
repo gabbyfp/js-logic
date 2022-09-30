75d91eb49957173dad8d393e85e9660c5bada48d
Gabby Pascual <gabbypascual@Gabbys-MBP.home>
2022-09-29 15:28:03 -1000

    assignment pau

--- exercises.js
+++ exercises.js
@@ -11,21 +11,42 @@ else
 This function will return the statement 'Bring an umbrella' if the boolean value is true.
 */
 
+function isRainy(boo){
+    if(boo === true){
+        return 'Bring an umbrella';
+    }
+}
 
+console.log(isRainy(true));
 
 /*2. Create a function named vaccinated which takes a boolean parameter boo. 
 If the value is true, it'll return the statement: 'Please come in.' 
 If the value is false, it'll return the statement: 'Please show me your Covid test.'
 */
 
+function vaccinated(boo){
+    if(boo === true){
+        return 'Please come in.';
+    }if(boo === false){
+        return 'Please show me your Covid test.';
+    }
+}
 
+console.log(vaccinated(true));
+console.log(vaccinated(false));
 
 
 /*3. Create a function named scrabble which takes two string parameters word1, word2. 
 If word1 is longer than word2, it will return the statement: 'premium score'. 
 If not, it will return 'normal score'.
 */
-
+function scrabble(word1, word2){
+    if(word1.length > word2.length){
+        return 'premium score';
+    }else{return 'normal score'};
+}
+console.log(scrabble('watermelon', 'sugar'));
+console.log(scrabble('harry', 'styles'));
 
 
 
@@ -35,12 +56,34 @@ If the budget is over 50, return 'Gap';
 If the budget is 50 or less, return 'Old Navy';
 */
 
+function shopping(budget){
+    if(budget>100){
+        return 'Banana Republic';
+      }else if (budget>50)
+      {return 'Gap';}
+    else{
+        return 'Old Navy'};
+  }
+
+console.log(shopping(50));
+console.log(shopping(60));
+console.log(shopping(125));
 
 
 /*5. Create a function named bouncer which takes two number parameters, age, cover.
 If the age is greater than or equal to 21 AND if the cover is greater than or equal to 10, return 'Please enter'
 If not, return 'Sorry buddy'
 */
+function bouncer(age, cover){
+    if(age>=21 & cover>=10){
+        return 'Please enter';
+    }else{
+        return 'Sorry buddy'
+    }
+}
+
+console.log(bouncer(19, 20));
+console.log(bouncer(25, 10));
 
 
 
@@ -48,19 +91,30 @@ If not, return 'Sorry buddy'
 If the number is even, it'll return true. 
 If not, it'll return false.
 */
-
-
+function even(num){
+    if(num %2 == 0){
+        return 'True';
+    }else{
+        return 'False';
+    }
+}
+console.log(even(66));
+console.log(even(45));
 
 
 //Loops allow you to do the same action(s) on every item in a list
 
 //1. Create a for loop that will print number 0 - 10
-
-
+for(let i = 0; i<11; i++){
+    console.log(i)};
 
 //2. Create a for loop that will iterate and print each element in the array below.
 
-let cookies = ['peanut butter', 'chocolate chip', 'almond', 'oreos', 'lemon'];
+let cookies = ['peanut butter', 'chocolate chip', 'almond', 'oreos', 'lemon']
+for(let i=0; i<cookies.length; i++){
+    console.log(cookies[i]);
+}
+
 
 /*3. Create a for loop that will iterate each element in the array and print the following:
 Player 0 is Tatum.
@@ -72,11 +126,19 @@ Player 5 is Kidd.
 */
 
 let players = ['Tatum', 'Hardaway', 'Leonard', 'Paul', 'Webber', 'Kidd'];
+for (let i=0; i<players.length; i++){
+    console.log("Player " + i + " is " + players[i] + '.');
+}
 
 //4. Create a for loop that will push 20 odd numbers to the oddNum array. Start from 1. 
 
 let oddNum = [];
-
+for (let i=1; i < 40; i++){
+    if(i%2 == 1){
+        oddNum.push(i);
+    }
+}
+console.log(oddNum);
 
 
 
@@ -86,12 +148,19 @@ let rappers = ['DMX', 'Snoop', 'Jay-Z', 'Dre', 'Nas', 'Tupac', 'Biggie', 'Ice Cu
 let eastSide = [];
 let westSide = [];
 
-
+for (let i=0; i<rappers.length; i++){
+    if(i%2 == 0){eastSide.push(rappers[i]);
+    }else{westSide.push(rappers[i])};
+}
+console.log(eastSide, westSide);
 
 //6. Create a for loop that will sum up numbers from 1 to 100 and store the value in the sum variable.
 
 let sum = 0;
-
+for (let i=1; i<101; i++){
+    sum+=i;
+}
+console.log(sum);
 
 
 /*7. Create a for loop that will print numbers from 0 to 60, but will print:
@@ -99,5 +168,14 @@ let sum = 0;
 'Maia' if the number is divisible by 5
 'Purple Maia' if the number is divisible by both 3 and 5
 */
+for(let i = 0; i < 61; i++){
+    if (i%15 === 0){
+        console.log('Purple Maia');
+    }else if (i%3 === 0){
+        console.log('Purple');
+    }else if (i%5 === 0){
+        console.log('Maia');
+    }else{console.log(i);}
+}
 
 
